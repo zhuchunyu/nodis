09dc32b4f46d934e12bac28d0b0745650dbb0f3a
zhuchunyu <zhuchunyu_14a@163.com>
2018-05-03 18:03:06 +0800

    feat: functions

--- file/njk.js
+++ file/njk.js
@@ -1,4 +1,6 @@
 const nunjucks = require('nunjucks')
+const fs = require('fs')
+const archiver = require('archiver')
 
 nunjucks.configure({
   autoescape: true,
@@ -30,3 +32,47 @@ const shtml = nunjucks.render('index.nunjucks', {
 });
 
 console.log(shtml)
+
+// create a file to stream archive data to.
+const output = fs.createWriteStream(__dirname + '/mcu-sdk.zip');
+const archive = archiver('zip', {
+  zlib: { level: 9 } // Sets the compression level.
+});
+
+// listen for all archive data to be written
+// 'close' event is fired only when a file descriptor is involved
+output.on('close', function() {
+  console.log(archive.pointer() + ' total bytes');
+  console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+// This event is fired when the data source is drained no matter what was the data source.
+// It is not part of this library but rather from the NodeJS Stream API.
+// @see: https://nodejs.org/api/stream.html#stream_event_end
+output.on('end', function() {
+  console.log('Data has been drained');
+});
+
+// good practice to catch warnings (ie stat failures and other non-blocking errors)
+archive.on('warning', function(err) {
+  if (err.code === 'ENOENT') {
+    // log warning
+  } else {
+    // throw error
+    throw err;
+  }
+});
+
+// good practice to catch this error explicitly
+archive.on('error', function(err) {
+  throw err;
+});
+
+// pipe archive data to the file
+archive.pipe(output);
+
+// append a file from buffer
+const buffer3 = Buffer.from(shtml);
+archive.append(buffer3, { name: 'file3.h' });
+
+archive.finalize();
